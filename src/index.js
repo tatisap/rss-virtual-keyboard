93f1450f5757a51c16bc99b3665513a84ae0c59b
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-05-05 23:48:49 +0300

    feat: implement buttons adding

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './styles/style.scss';
 import initPageLayout from './scripts/page';
+import Button from './scripts/button';
+import keys from './keys.json';
 
 initPageLayout();
+
+function addKeys() {
+  keys.forEach((key) => {
+    new Button(key).addToBoard();
+  });
+}
+
+addKeys();
+
+const arrowWrapper = document.createElement('div');
+arrowWrapper.classList.add('arrow-wrapper');
+document.querySelector('.keyboard-wrapper').append(arrowWrapper);
+document.querySelectorAll('.arrow').forEach((arrow) => arrowWrapper.append(arrow));
---
+++ src/scripts/button.js
@@ -0,0 +1,28 @@
+export default class Button {
+  constructor(key) {
+    const element = document.createElement('div');
+    element.classList.add('button');
+
+    if (key.type === 'functional') {
+      element.classList.add(key.position);
+      const keyClass = key.enKey.split('').map((i) => ((i === ' ') ? '-' : i)).join('');
+      element.classList.add(keyClass);
+    }
+
+    if (key.code === 'Space') element.classList.add('space');
+    if (key.code.includes('Arrow')) element.classList.add('arrow');
+
+    const content = document.createElement('span');
+    content.textContent = key.enKey;
+    element.append(content);
+
+    this.element = element;
+    this.content = content;
+    this.code = key.code;
+    this.type = key.type;
+  }
+
+  addToBoard() {
+    document.querySelector('.keyboard-wrapper').append(this.element);
+  }
+}
